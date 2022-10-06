0b6e780737920e59fe2d3db8e78b9305f0d75cbf
usama <mahmoodify@gmail.com>
2022-10-06 15:06:54 +0500

    correcting bug for file listing

--- src/converter/providers/pathrecursion.py
+++ src/converter/providers/pathrecursion.py
@@ -22,38 +22,43 @@ from os.path            import isfile
 from os.path            import isdir
 from os.path            import join
 
-from settings import Settings
+from providers.settings import Settings
 
 
 def getDirectories(directory, recursion = False, depth = 0):
     try:
+        if isdir(directory):
+            yield directory
+        
         for name in listdir(directory):
             full_name = join(directory, name)
-            if(isdir(full_name)):
+            if isdir(full_name):
                 yield full_name
 
-                if(recursion and depth > 0):
+                if recursion and depth > 0:
                     yield from getDirectories(full_name, recursion, depth - 1)
-    except:
-        return
+    except: pass
 
 def getFiles(directory, extension = ""):
     try:
         for name in listdir(directory):
             full_name = join(directory, name)
-            if(isfile(full_name)):
-                if (extension == None or extension == ""):
+            if isfile(full_name):
+                if extension == None or extension == "":
                     yield full_name
                 else:
-                    if(full_name.lower().endswith(extension.lower())):
+                    if full_name.lower().endswith(extension.lower()):
                         yield full_name
     except:
         return f"<cannot access - {directory}>"
 
 def getDesiredFiles(settings: Settings, extension):
-    for dir in getDirectories(
-                        settings.scanDir,
-                        settings.isRecursive,
-                        settings.recursionDepth):
-        for full_filename in getFiles(dir, extension):
-            yield full_filename
+    if isfile(settings.scanDirOrFile):
+        yield settings.scanDirOrFile
+    else:
+        for dir in getDirectories(
+                            settings.scanDirOrFile,
+                            settings.isRecursive,
+                            settings.recursionDepth):
+            for full_filename in getFiles(dir, extension):
+                yield full_filename
