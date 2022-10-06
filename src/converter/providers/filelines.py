b0fbe107e4daab5327b4ab90f78ae4675e120f5d
usama <mahmoodify@gmail.com>
2022-10-06 14:09:05 +0500

    adding getlines

--- src/converter/providers/filelines.py
+++ src/converter/providers/filelines.py
@@ -16,3 +16,18 @@
 # *
 # *
 # *****************************************************************************************
+
+from os.path            import isfile
+
+
+def getLines(filename: str, filterEmptyLines: bool = False):
+    if isfile(filename):
+        ln = 0
+        for l in open(filename):
+            ln += 1
+
+            if filterEmptyLines == False:
+                yield (l, ln)
+            else:
+                if len(l.strip()) > 0:
+                    yield (l, ln)
