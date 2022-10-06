a4d3e0e02e6323213b9fc0ea3e35f28f70eebb64
usama <mahmoodify@gmail.com>
2022-10-06 14:23:23 +0500

    outlining pipeline

--- src/converter/processing/processing_pipeline.py
+++ src/converter/processing/processing_pipeline.py
@@ -16,3 +16,24 @@
 # *
 # *
 # *****************************************************************************************
+
+from ..providers.settings import Settings
+
+
+class Pipeline:
+    def __init__(self, settings: Settings) -> None:
+        self.settings: Settings = settings
+        self.isPipelineSet: bool = False
+
+        self.setPipeline()
+
+    def setPipeline(self) -> None:
+        if self.isPipelineSet:
+            self.isPipelineSet = True
+
+            ##########
+            # Pipeline Setup
+            #####
+
+    def proceed(self) -> None:
+        pass
