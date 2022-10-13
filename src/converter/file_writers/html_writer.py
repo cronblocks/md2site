c3ed8cd5a60908111d314e36bcdc7f87b52714cb
usama <mahmoodify@gmail.com>
2022-10-13 22:56:00 +0500

    adding settings

--- src/converter/file_writers/html_writer.py
+++ src/converter/file_writers/html_writer.py
@@ -17,10 +17,11 @@
 # *
 # *****************************************************************************************
 
+from providers.settings           import Settings
 from file_writers.base_writer     import BaseWriter
 from processing.data              import ProcessedData
 
 
 class HtmlWriter(BaseWriter):
-    def __init__(self, data: ProcessedData) -> None:
+    def __init__(self, settings: Settings) -> None:
         pass
