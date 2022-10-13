4f7cbde2dbdaae146e4dcf2160b221838c465c06
usama <mahmoodify@gmail.com>
2022-10-13 22:59:11 +0500

    adding writing of data

--- src/converter/file_writers/base_writer.py
+++ src/converter/file_writers/base_writer.py
@@ -18,8 +18,12 @@
 # *****************************************************************************************
 
 from providers.settings import Settings
+from processing.data    import ProcessedData
 
 
 class BaseWriter:
     def __init__(self, settings: Settings) -> None:
         self.settings: Settings = settings
+    
+    def writeData(self, data: ProcessedData) -> None:
+        pass
